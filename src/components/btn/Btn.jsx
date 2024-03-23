c7cbe0772f33214af4e325c7e0c943d5410dda0e
JiJi9526 <maynine952006@gmail.com>
2024-03-23 23:49:42 +0630

    network update

---
+++ src/components/btn/Btn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Btn = ({text}) => {
+  return (
+    <div>
+        <button className="bg-[#D9D9D9]  py-1 w-full text-lg rounded-2xl">
+           {text}
+        </button>
+    </div>
+  )
+}
+
+export default Btn
