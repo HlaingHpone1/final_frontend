8030dd70b88e3dd01be0bf074071470765481902
JiJi9526 <maynine952006@gmail.com>
2024-03-26 10:37:06 +0630

    Reset Password Form

---
+++ src/components/emailResetForm/EmailResetForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const EmailResetForm = () => {
+    const [data, setData] = useState({
+        email: "",
+    });
+
+    const inputHandler = (e) => {
+        const { name, value } = e.target;
+        setData({
+            ...data,
+            [name]: value.trim(),
+        });
+    };
+    return (
+        <section>
+            <form
+                className=" min-w-[350px]"
+                action=""
+                // onSubmit={submitHandler}
+                >
+                <div className="input-box mb-5 ">
+                    <label className='text-lg'>Email</label>
+                    <input
+                        className={` bg-transparent border-2  focus:border-slate-700 transition-colors duration-200 ease-linear w-full block text-lg px-2 py-2.5 rounded-sm mt-2 `}
+                        type="email"
+                        name="email"
+                        // id="email"
+                        value={data.email}
+                        placeholder='Enter your email'
+                        onChange={inputHandler}
+                        autoComplete="off"
+                    />
+                </div>
+                <button
+                    className="bg-slate-500 text-white px-5 py-2 rounded-md text-lg"
+                    type="submit">
+                    Submit
+                </button>
+            </form>
+        </section>
+    )
+}
+
+export default EmailResetForm
---
+++ src/components/resetWthOldpsw/OldPswForm.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react'
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const OldPswForm = () => {
+    const [data, setData] = useState({
+        oldPassword: "",
+        newPassword: "",
+        confirmNewPassword: "",
+    });
+
+    const [showPassword, setShowPassword] = useState({
+        oldPassword: "",
+        newPassword: "",
+        confirmNewPassword: "",
+    });
+
+    const inputHandler = (e) => {
+        const { name, value } = e.target;
+        setData({
+            ...data,
+            [name]: value.trim(),
+        });
+    };
+
+    const showPasswordVisibility = (field) => {
+        setShowPassword({
+            ...showPassword,
+            [field]: !showPassword[field],
+        });
+    };
+    return (
+        <section>
+            <form
+                className=" min-w-[350px]"
+                action=""
+            // onSubmit={submitHandler}
+            >
+
+                {/* oldPassword */}
+                <div className="input-box mb-3">
+                    <div
+                        className={`flex items-center border-b-2 transition-colors duration-200 ease-linear  focus:border-slate-700`}
+                    >
+                        <input
+                            className={`focus:outline-none bg-transparent  w-full block text-lg px-2 py-2.5 `}
+                            type={showPassword.password ? "text" : "password"}
+                            name="oldPassword"
+                            value={data.password}
+                            onChange={inputHandler}
+                            placeholder="Enter Old Password"
+                            autoComplete="off"
+                        />
+                        <button
+                            type="button"
+                            onClick={() => showPasswordVisibility("password")}
+                        >
+                            {showPassword.password ? (
+                                <FaEye className="opacity-75" size={20} />
+                            ) : (
+                                <FaEyeSlash className="opacity-25" size={20} />
+                            )}
+                        </button>
+                    </div>
+                </div>
+
+
+                {/* newPassword */}
+                <div className="input-box mb-3">
+                    <div
+                        className={`flex items-center border-b-2 transition-colors duration-200 ease-linear  focus:border-slate-700`}
+                    >
+                        <input
+                            className={`focus:outline-none bg-transparent  w-full block text-lg px-2 py-2.5 `}
+                            type={showPassword.password ? "text" : "password"}
+                            name="newPassword"
+                            value={data.newPassword}
+                            onChange={inputHandler}
+                            placeholder="New Password"
+                            autoComplete="off"
+                        />
+                        <button
+                            type="button"
+                            onClick={() => showPasswordVisibility("password")}
+                        >
+                            {showPassword.password ? (
+                                <FaEye className="opacity-75" size={20} />
+                            ) : (
+                                <FaEyeSlash className="opacity-25" size={20} />
+                            )}
+                        </button>
+                    </div>
+                </div>
+
+
+                {/* confirm new password */}
+                <div className="input-box mb-3">
+                    <div
+                        className={`flex items-center border-b-2 transition-colors duration-200 ease-linear  focus:border-slate-700`}
+                    >
+                        <input
+                            className={`focus:outline-none bg-transparent  w-full block text-lg px-2 py-2.5 `}
+                            type={showPassword.password ? "text" : "password"}
+                            name="confirmNewPassword"
+                            value={data.confirmNewPassword}
+                            onChange={inputHandler}
+                            placeholder="Confirm Password"
+                            autoComplete="off"
+                        />
+                        <button
+                            type="button"
+                            onClick={() => showPasswordVisibility("password")}
+                        >
+                            {showPassword.password ? (
+                                <FaEye className="opacity-75" size={20} />
+                            ) : (
+                                <FaEyeSlash className="opacity-25" size={20} />
+                            )}
+                        </button>
+                    </div>
+                </div>
+
+                <button
+                    className="bg-slate-500 text-white px-5 py-2 rounded-md text-lg"
+                    type="submit">
+                    Submit
+                </button>
+            </form>
+        </section>
+    )
+}
+
+export default OldPswForm
