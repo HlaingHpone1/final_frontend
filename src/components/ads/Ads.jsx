585ae212e0c0cc21bf9bec6599308a10d148798d
leosharing <leoknowledgesharing@gmail.com>
2024-04-01 11:35:50 +0630

    Refactor Ads component and update ad names

--- src/components/ads/Ads.jsx
+++ src/components/ads/Ads.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { img } from "../img";
 import { Link } from "react-router-dom";
 
-export const Ads = () => {
+export const ShortAds = () => {
     return (
         <div>
             <img
@@ -18,34 +18,42 @@ export const SubscriptionAds = () => {
     return (
         <div className=" w-80 bg-gray-600 rounded-xl">
             <div className="p-4 text-white">
-                <p className="font-medium text-lg">Grow Your Network with Premium</p>
-                <p className="text-sm py-4">Premium InMail is 4.6x more effective in hearing back than cold email.</p>
+                <p className="font-medium text-lg">
+                    Grow Your Network with Premium
+                </p>
+                <p className="text-sm py-4">
+                    Premium InMail is 4.6x more effective in hearing back than
+                    cold email.
+                </p>
 
-                <Link to={"/subscription"}
-                className="py-2 px-5 bg-yellow-200 text-black font-semibold rounded-3xl " >
+                <Link
+                    to={"/subscription"}
+                    className="py-2 px-5 bg-yellow-200 text-black font-semibold rounded-3xl "
+                >
                     Try Premium for free
                 </Link>
 
                 <p className="text-sm text-gray-200 pt-4">
-                    1-month free trial. We'll send you a reminder 7 days before your trial ends.</p>
+                    1-month free trial. We'll send you a reminder 7 days before
+                    your trial ends.
+                </p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export const LongAds = ()=>{
+export const LongAds = () => {
     return (
         <div className="">
             <img src={img.longads2} className=" rounded-2xl mb-5 h-[500px]" />
         </div>
-    )
-}
+    );
+};
 
-export const MediumAds = ()=>{
+export const MediumAds = () => {
     return (
         <div className="">
             <img src={img.mediumads} className=" rounded-2xl mb-5 h-[400px]" />
         </div>
-    )
-}
-
+    );
+};
